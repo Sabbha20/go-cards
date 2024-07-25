d7b01b38f123d9dcf7582bbd9387e4d81e2884ca
Sabbha20 <sabbha.power1@gmail.com>
2024-07-26 01:08:21 +0530

    Different ways of declaring a variable

--- main.go
+++ main.go
@@ -5,4 +5,16 @@ import "fmt"
 func main(){
 	var card string = "Ace of Spades"
 	fmt.Println(card)
+	// Another method of initialization
+	card1 := "Five of Diamonds"
+	// Since the variable is already intialized, so we are not using colon(:), just assigning =
+	card1 = "Six of Hearts" 
+	fmt.Println(card1) 
+
+	card2 := newCard()
+	fmt.Println(card2)
+}
+
+func newCard() string{
+	return "Ace of Clubs"
 }
