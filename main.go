5d67ecc7a36b7516e68a5f26c5fa217fd84cf1d5
Sabbha20 <sabbha.power1@gmail.com>
2024-08-09 10:34:56 +0530

    renamed the cards

--- main.go
+++ main.go
@@ -3,19 +3,19 @@ package main
 import "fmt"
 
 func main(){
-	var card string = "Ace of Spades"
+	var card string = "A♠️"
 	fmt.Println(card)
 	// Another method of initialization
-	card1 := "Five of Diamonds"
+	card1 := "5♦️"
 	// Since the variable is already intialized, so we are not using colon(:), just assigning =
-	card1 = "Six of Hearts" 
+	card1 = "6❤️" 
 	fmt.Println(card1) 
 
 	card2 := newCard()
 	fmt.Println(card2)
 
 	cards:= [] string {}
-	cards = append(cards, "Five of Diamonds", card, card1, newCard() )
+	cards = append(cards, "5♦️", card, card1, newCard() )
 	fmt.Println(cards)
 	for i, crd:= range cards{
 		fmt.Println(i, crd)
@@ -23,5 +23,5 @@ func main(){
 }
 
 func newCard() string{
-	return "Ace of Clubs"
+	return "A♣️"
 }
